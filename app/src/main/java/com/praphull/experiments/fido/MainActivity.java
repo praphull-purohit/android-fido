359e678ee0af464c20646fc88379cfa4c54195ba
Praphull <1288395+praphull@users.noreply.github.com>
2021-03-17 14:42:57 +0530

    Implement onActivityResult

--- app/src/main/java/com/praphull/experiments/fido/MainActivity.java
+++ app/src/main/java/com/praphull/experiments/fido/MainActivity.java
@@ -1,12 +1,14 @@
 package com.praphull.experiments.fido;
 
 import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.common.util.Strings;
 import com.google.android.gms.fido.Fido;
 import com.google.android.gms.fido.fido2.Fido2ApiClient;
+import com.google.android.gms.fido.fido2.api.common.AuthenticatorAssertionResponse;
+import com.google.android.gms.fido.fido2.api.common.AuthenticatorAttestationResponse;
+import com.google.android.gms.fido.fido2.api.common.AuthenticatorErrorResponse;
 import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialCreationOptions;
 import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialRequestOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
+import com.google.common.io.BaseEncoding;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
@@ -26,7 +32,7 @@ import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
     private static final int REGISTER_REQUEST_CODE = 0;
-    private static final int SIGN_REQUEST_CODE = 1;
+    private static final int LOGIN_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -142,7 +148,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     startIntentSenderForResult(
                             pendingIntent.getIntentSender(),
-                            SIGN_REQUEST_CODE,
+                            LOGIN_REQUEST_CODE,
                             null,
                             0,
                             0,
@@ -153,5 +159,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         result.addOnFailureListener(e -> show("Failed to login: " + e.getMessage(), e));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (data.hasExtra(Fido.FIDO2_KEY_ERROR_EXTRA)) {
+                AuthenticatorErrorResponse response =
+                        AuthenticatorErrorResponse.deserializeFromBytes(
+                                data.getByteArrayExtra(Fido.FIDO2_KEY_ERROR_EXTRA));
+                show("Received error response from Google Play Services FIDO2 API: " + response);
+            } else if (requestCode == REGISTER_REQUEST_CODE) {
+                show("Received register response from Google Play Services FIDO2 API");
+                AuthenticatorAttestationResponse response =
+                        AuthenticatorAttestationResponse.deserializeFromBytes(
+                                data.getByteArrayExtra(Fido.FIDO2_KEY_RESPONSE_EXTRA));
+                updateRegisterResponseToServer(response);
+            } else if (requestCode == LOGIN_REQUEST_CODE) {
+                show("Received login response from Google Play Services FIDO2 API");
+                AuthenticatorAssertionResponse response =
+                        AuthenticatorAssertionResponse.deserializeFromBytes(
+                                data.getByteArrayExtra(Fido.FIDO2_KEY_RESPONSE_EXTRA));
+                updateLoginResponseToServer(response);
+            }
+        } else {
+            show("Operation failed!");
+            Toast.makeText(
+                    MainActivity.this,
+                    "Operation failed, with resultCode " + resultCode,
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    private void updateRegisterResponseToServer(AuthenticatorAttestationResponse response) {
+        //TODO
+    }
+
+    private void updateLoginResponseToServer(AuthenticatorAssertionResponse response) {
+        //TODO
     }
 }
